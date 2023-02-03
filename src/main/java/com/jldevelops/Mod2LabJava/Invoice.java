ed88dbcec6bc09088612631cffa53eb8d5bd16d1
Johnson Li <liwindx15@gmail.com>
2023-02-02 23:31:11 -0500

    adding classes made to github

---
+++ src/main/java/com/jldevelops/Mod2LabJava/Invoice.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.jldevelops.Mod2LabJava;
+
+/**
+ *
+ * @author Johnson
+ */
+public class Invoice {
+
+    private String number;
+    private String description;
+    private int quantity;
+    private double pricePerItem;
+
+    public Invoice(String number, String description, int quantity, double pricePerItem) {
+        this.number = number;
+        this.description = description;
+        this.quantity = quantity;
+        this.pricePerItem = pricePerItem;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPricePerItem() {
+        return pricePerItem;
+    }
+
+    public void setPricePerItem(double pricePerItem) {
+        if (pricePerItem > 0) {
+            this.pricePerItem = pricePerItem;
+        } else {
+            this.pricePerItem = 0.0;
+        }
+    }
+
+    public double getInvoiceAmount() {
+        return quantity * pricePerItem;
+    }
+}
